ef4a9fcbc4a9bfc84bb27f93402cb90f7ddd2d6e
stellasia <stellasia@devience.fr>
2015-06-22 19:24:51 +0200

    Add python startup script

---
+++ startup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Python startup script. 
+
+Include in your .zshrc (or equivalent): 
+
+export PYTHONSTARTUP=/path/to/this/file/startup.py
+
+"""
+
+import os 
+import numpy as np
+import pandas as pd
+ 
+  
